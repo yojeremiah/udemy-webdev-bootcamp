3979baca93f380fdc92c0a4b44524bfe6537c061
Jeremiah S <yojeremiahhh@gmail.com>
2017-07-24 14:32:06 -0700

    finished 'Intro to jQuery' section

---
+++ 16-Intro-to-jQuery/jqueryIntro.js
@@ -0,0 +1,9 @@
+// jQuery selector exercise
+// select all divs and give them purple bkgd
+$("div").css("background", "purple");
+// select class highlight divs and make them 200px wide
+$(".highlight").css("width", "200px");
+// select id 'third' div and give it orange border
+$("#third").css("border", "2px solid orange");
+// select only first div and change font color to pink
+$("div").first().css("color", "pink");
